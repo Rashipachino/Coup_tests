053f7c2c603949d9c957e3e65b03210e5e94c263
Rashipachino <93292546+Rashipachino@users.noreply.github.com>
2022-04-26 18:07:35 +0300

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,17 +1,15 @@
 #include "Game.hpp"
 
-string coup::Game::turn(){
-    return "bla";
-}
-vector<string> coup::Game::players(){
-    vector<string> ex;
-     for (unsigned long i = 0; i < 10; i++)
-     {
-         ex[i] = "hi";
-     }
-    return ex;
-}
-string coup::Game::winner()//if game is still active, throw error
-{
-    return "winner";
+namespace coup{
+    string coup::Game::turn(){
+        return "bla";
+    }
+    vector<string> coup::Game::players(){
+        vector<string> ex;
+        return ex;
+    }
+    string coup::Game::winner()//if game is still active, throw error
+    {
+        return "winner";
+    }
 }
