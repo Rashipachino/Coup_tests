58cdfcc28fe6cb31ee9c4cd254ffee9d5c9c951d
Rashipachino <93292546+Rashipachino@users.noreply.github.com>
2022-04-26 18:06:16 +0300

    Update Duke.hpp

--- Duke.hpp
+++ Duke.hpp
@@ -6,7 +6,9 @@ using namespace std;
 class Duke : public Player
 {
     public:
-        Duke(Game g, string name);   
+        Duke(Game game, string name) : Player(game, name) {
+            
+        }
         string role();
         void tax();
         void block(Player p);
