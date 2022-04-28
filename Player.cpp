106a01822c529bd97f3a0ade64e85f209f50cb44
Rashipachino <93292546+Rashipachino@users.noreply.github.com>
2022-04-28 10:43:20 +0300

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -2,7 +2,10 @@
 
 void Player::income(){}
 void Player::foreign_aid(){}
-void Player::coup(Player p){}
-int Player::coins(){
+void Player::coup(const Player &p){}
+int Player::coins() const{
     return coin_count;
+}
+string Player::role(){
+    return "Player";
 }
