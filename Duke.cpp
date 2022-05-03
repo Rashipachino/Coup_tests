b1407b7e12fc296bad3235673781fea990ac8ec7
Rashipachino <93292546+Rashipachino@users.noreply.github.com>
2022-05-03 19:27:44 +0300

    Update Duke.cpp

--- Duke.cpp
+++ Duke.cpp
@@ -2,7 +2,7 @@
 #include "Game.hpp"
 
 void Duke::tax(){ //add in turn
-if(this->game.turn() != this->name){
+if(this->game->turn() != this->name){
         throw invalid_argument("Player is out of turn");
     }
 else if(coins() == 10){
@@ -10,18 +10,22 @@ else if(coins() == 10){
 }
 else{
     set_coins(coins() + 3);
+    this->game->update_turn();
 }
 
 }
 void Duke::block(Player &p){//blocks foreign aid 
-    if(p.get_game().get_status(p.get_name()) == "Dead"){
+    if(p.get_game()->get_status(p.get_name()) == "Dead"){
         throw invalid_argument("Player is dead. Cannot block");
     }
-    else if(p.get_last_move() != "foreign_aid"){
+    else if(p.get_last_move() != "Foreign_aid"){
         throw invalid_argument("Too late Duke");
     }
     else{
         p.set_coins(p.coins()-2);
-        p.set_last_move("foreign_aid_blocked");
+        p.set_last_move("Foreign_aid_blocked");
     }
+}
+string Duke::role(){
+    return "Duke";
 }
