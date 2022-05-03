8a5e4c665f62eb8534fe04242d6af3b69def508b
Rashipachino <93292546+Rashipachino@users.noreply.github.com>
2022-05-03 19:23:38 +0300

    Update Ambassador.cpp

--- Ambassador.cpp
+++ Ambassador.cpp
@@ -2,24 +2,24 @@
 #include "Game.hpp"
 
     void Ambassador::transfer(Player &p1, Player &p2){ //can transfer points from one player to another
-        if(this->game.turn() != this->name){ //not this player's turn
+        if(this->game->turn() != this->name){ //not this player's turn
             throw invalid_argument("Player is out of turn");
         }
         if(coins() == 10){ //player must coup with 10 coins in hand
             throw invalid_argument("Player must coup");
         }
-        if(p1.get_game().get_status(p1.get_name()) == "Dead" || p2.get_game().get_status(p2.get_name()) == "Dead"){ //one or both of players are dead
+        if(p1.get_game()->get_status(p1.get_name()) == "Dead" || p2.get_game()->get_status(p2.get_name()) == "Dead"){ //one or both of players are dead
             throw invalid_argument("Player is dead, therefore, cannot be transfered to/from");
         }
         else{
             p1.set_coins(p1.coins()-1);
             p2.set_coins(p2.coins()+1);
             this->last_move = "Transfer";
-            this->game.update_turn();
+            this->game->update_turn();
         }
-    };
+    }
     void Ambassador::block(Player &p1){ //can only block captain
-        if(p1.get_game().get_status(p1.get_name()) != "Alive"){
+        if(p1.get_game()->get_status(p1.get_name()) != "Alive"){
             throw invalid_argument("Player is dead or almost dead, cannot be blocked");
         }
         if(p1.role() != "Captain"){
@@ -32,4 +32,7 @@
             p1.set_coins(p1.coins()-2); //reduce captain's coin count;
             
         }
-    };
+    }
+    string Ambassador::role(){
+        return "Ambassador";
+    }
