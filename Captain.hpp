b4af0ada9905d70a123f43a43ebb0a2cb9f64d12
Rashipachino <93292546+Rashipachino@users.noreply.github.com>
2022-05-03 19:26:22 +0300

    Update Captain.hpp

--- Captain.hpp
+++ Captain.hpp
@@ -5,8 +5,9 @@ using namespace std;
 class Captain : public Player
 {
     public:
-        Captain(Game game, string name) : Player(game, name) {
+        Captain(Game &game, string name) : Player(game, name) {
         }
         void block(Player &p); //can block like ambassador
         void steal(Player &p);
+        string role() override;
 };
