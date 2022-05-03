633afe5fff415200448e076fe7097ab10edcc9e5
Rashipachino <93292546+Rashipachino@users.noreply.github.com>
2022-05-03 19:24:55 +0300

    Update Assassin.cpp

--- Assassin.cpp
+++ Assassin.cpp
@@ -6,22 +6,26 @@ void Assassin::coup(Player &p) {
     if(this->coin_count >= 10){ //regular coup
         Player::coup(p);
     }
-    else if(this->game.turn() != this->name){
+    else if(this->game->turn() != this->name){
         throw invalid_argument("Player is out of turn");
     }
     else if(this->coin_count < 3){
         throw invalid_argument("Not enough coins to coup");
     }
-    else if(p.get_game().get_status(p.get_name()) == "Dead"){
+    else if(p.get_game()->get_status(p.get_name()) == "Dead"){
         throw invalid_argument("Player to coup is already dead");
     }
     else{
-        p.get_game().set_status(p.get_name(), "Assassinated"); //Assassinate character
+        p.get_game()->set_status(p.get_name(), "Assassinated"); //Assassinate character
         this->coin_count -= 3;
         this->last_move = "Coup";
-        this->game.update_turn();
+        this->game->update_turn();
         this->history = &p;
     }
+}
+
+string Assassin::role(){
+    return "Assassin";
 }
     
 
