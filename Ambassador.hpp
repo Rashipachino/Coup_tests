14524852b4b8f32bcd57f35a0198c6d310cd0758
Rashipachino <93292546+Rashipachino@users.noreply.github.com>
2022-05-03 19:24:06 +0300

    Update Ambassador.hpp

--- Ambassador.hpp
+++ Ambassador.hpp
@@ -5,8 +5,9 @@ using namespace std;
 class Ambassador : public Player
 {
     public:
-        Ambassador(Game game, string name) : Player(game, name) {
+        Ambassador(Game& game, string name) : Player(game, name) {
         }
         void transfer(Player &p1, Player &p2);
         void block(Player &p1);
+        string role() override;
 };
